c8a3c79c3615b74cc22e0e3a7b372e7ed26ccf9f
Angie <angie@genosha.com.ar>
2020-07-22 17:47:25 -0300

    Change name and delete unused dependency

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Unicon from "vue-unicons";
-import { uniEdit, uniSave, uniPlus } from "vue-unicons/src/icons";
-import UniqueId from "vue-unique-id";
+import { uniEdit, uniSave, uniPlus, uniTrashAlt } from "vue-unicons/src/icons";
 
-Unicon.add([uniEdit, uniSave, uniPlus]);
+Unicon.add([uniEdit, uniSave, uniPlus, uniTrashAlt]);
 Vue.use(Unicon);
-Vue.use(UniqueId);
 Vue.config.productionTip = false;
 
 new Vue({
